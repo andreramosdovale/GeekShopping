e0607b25d27eface12426b809695473f1497e8c3
andreramosdovale <andre_ramosdovale@outlook.com>
2023-01-05 23:06:56 -0300

    finish product crud

--- GeekShoping.ProductApi/Controllers/ProductController.cs
+++ GeekShoping.ProductApi/Controllers/ProductController.cs
@@ -1,4 +1,5 @@
 ﻿using GeekShoping.ProductApi.Data.ProductDTO;
+using GeekShoping.ProductApi.Model;
 using GeekShoping.ProductApi.Repository;
 using Microsoft.AspNetCore.Mvc;
 
@@ -32,5 +33,35 @@ public class ProductController : ControllerBase
         if (product == null) return NotFound();
 
         return Ok(product);
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<ProductDTO>> Create(ProductDTO newProduct)
+    {
+        if (newProduct == null) return BadRequest();
+
+        var product = await _repository.Create(newProduct);
+
+        return Ok(product);
+    }
+
+    [HttpPut()]
+    public async Task<ActionResult<ProductDTO>> Update(ProductDTO product)
+    {
+        ProductDTO productUpdated = await _repository.Update(product);
+
+        if (productUpdated == null) return BadRequest();
+
+        return Ok(productUpdated);
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<ActionResult<ProductDTO>> Delete(long id)
+    {
+        var status = await _repository.DeleteById(id);
+
+        if (!status) return BadRequest();
+
+        return Ok();
     }
 }
